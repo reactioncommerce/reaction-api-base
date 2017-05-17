cf4cc79e1d6c6b5df800d14828e9ab800cd6c9bc
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-05-17 11:06:11 +0200

    update mongo-prebuilt and fix all breaking API changes

--- src/index.js
+++ src/index.js
@@ -1,48 +1,52 @@
 import fs from 'fs';
 import path from 'path';
 import nodemon from 'nodemon';
-import mongoPrebuilt from 'mongodb-prebuilt';
-import denodeify from 'denodeify';
+import { MongodHelper } from 'mongodb-prebuilt';
 import startServer from './server';
 import Logger from './server/logger';
-import MONGO_CODES from './utils/mongo-codes';
 
 // set the development database location
 const dbpath = path.join('../', 'db');
 
+// get the db/app configs from the env
 const {
   PORT = 3000,
   MONGO_PORT = parseInt(PORT, 10) + 2,
+  MONGO_DATABASE = 'api',
   MONGO_URL
 } = process.env;
 
+function startApp() {
+  startServer()
+    .then(() => Logger.info('Starting API server...'))
+    .catch((e) => Logger.error(e, 'Server startup error'));
+}
+
+// start a local development database if no MONGO_URL provided
 if (!MONGO_URL) {
   if (!fs.existsSync(dbpath)) {
     fs.mkdirSync(dbpath);
   }
 
-  // This promise never resolves if Mongo starts.
-  // However, we'll just go ahead and start the node server anyway,
-  // and if we see an error, we'll quit
-  denodeify(mongoPrebuilt.start_server.bind(mongoPrebuilt))({
-    auto_shutdown: true,
-    args: {
-      port: MONGO_PORT,
-      dbpath
-    }
-  })
-  .catch((errorCode) => {
-    const error = MONGO_CODES[errorCode];
-    Logger.error(`Failed to start MongoDB server on port ${MONGO_PORT}`);
-    Logger.error(`Error Code ${errorCode}: ${error ? error.longText : 'Unknown'}`);
-    process.exit(1);
-  });
-}
+  const mongod = new MongodHelper([
+    '--port', MONGO_PORT,
+    '--dbpath', dbpath
+  ]);
 
-// kick off the server
-startServer()
-  .then(() => Logger.info('All systems go!'))
-  .catch((e) => Logger.error(e, 'Server startup error'));
+  mongod.run().then(() => {
+  	Logger.info(`Mongod is now running at mongodb://localhost:${MONGO_PORT}/${MONGO_DATABASE}`);
+    startApp();
+  }, (e) => {
+    if (e === 'already running') {
+      startApp();
+    } else {
+      Logger.error(`Failed to start MongoDB server on port ${MONGO_PORT}. ${e}`);
+      process.exit(1);
+    }
+  });
+} else {
+  startApp();
+}
 
 // Ensure stopping our parent process will properly kill nodemon's process
 // Ala https://www.exratione.com/2013/05/die-child-process-die/
