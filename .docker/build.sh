6f2ef65966eeab036f0bdc28e37c07ee55086aa9
Ticean Bennett <ticean@reactioncommerce.com>
2018-02-06 13:49:26 -0800

    Local dev environment with simplified Docker setup
    Configures a Docker environment that can be used for development using
    Docker Compose.
    
    Removes external setup/cleanup scripts and brings everything directly
    into the Dockerfile. Now we invoke everything needed directly in the
    Dockerfile while retaining caching and other Docker optimizations.
    
    Using Yarn:
    
    In stage and production environments the yarn.lock file will be locked.
    
    In development, you will need to run the following command when the
    `package.json` file is modified:
    
        docker-compose run --rm api yarn install --modules-folder /opt/node_modules
        docker-compose build api
    
    This command will run `yarn install` inside the Docker container which
    will rebuild `yarn.lock` and install dependencies. Finally, rebuild the
    Docker image to install and cache the dependencies in the image.

--- .docker/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$1" == "--dev" ]; then
-  printf "\n[-] Building development image...\n\n"
-  docker build -f docker/dev.dockerfile -t reactioncommerce/reaction-api-base:devel .
-else
-  docker build -t reactioncommerce/reaction-api-base:latest .
-fi
